614ada3064e21bc6c88bc3bab438c3b84576dafc
Julio Figueiredo <juliofigueiredo08@gmail.com>
2025-12-16 15:02:42 -0100

    finished project features

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,19 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      console.log(action.payload)
+      state.items.push({...action.payload, quantity:1})
+      console.log("store:"+state.items)
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item)=> item.name !== action.payload.name)
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload
+      const existingCartItem = state.items.find(item=> item.name === name)
+      if (existingCartItem){
+        existingCartItem.quantity = quantity;
+      }
     },
   },
 });
